dfac90d383ce93b20d745b13404fbd90fa1e8f79
Aditya Nikam <adityanikam896@gmail.com>
2023-03-19 13:42:11 +0530

    git commit

---
+++ src/main/java/com/societymanagement/SocietyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.societymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyManagementApplication.class, args);
+    }
+
+}
