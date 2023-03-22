32e4a3e8c1fa597d5527dcd581ab010b03a3ee2d
Adinik896 <114090779+Adinik896@users.noreply.github.com>
2023-03-23 01:56:45 +0530

    Update Controller.java

--- src/main/java/com/societymanagement/controller/Controller.java
+++ src/main/java/com/societymanagement/controller/Controller.java
@@ -1,12 +1,47 @@
 package com.societymanagement.controller;
 
-import org.springframework.stereotype.Controller;
+import com.societymanagement.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class controller {
-    @GetMapping("/aditya")
-    public String layoutpg() {
-        return "layout.html";
+@org.springframework.stereotype.Controller
+public class Controller {
+/*    @Autowired
+    private MemberServices memberServices;*/
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @GetMapping("/home")
+    public String homePage() {
+        return "index.html";
+    }
+
+    @GetMapping("/delete")//TODO: will delete these once development is done.
+    public String delMember() {
+            memberRepository.deleteAll();
+        return "memberList.html";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login.html";
+    }
+
+    @GetMapping("/visitor")
+    public String visitorPage() {
+        return "visitor.html";
+    }
+
+
+    @GetMapping("/member-list")
+    public String list(Model model){
+        model.addAttribute("ganpat", memberRepository.findAll());
+      /*  ModelAndView  obj = new ModelAndView();
+        obj.getModelMap().put("members",model);
+        obj.setViewName("memberList.html");*/
+        return "memberList.html";
     }
 }
+
+
